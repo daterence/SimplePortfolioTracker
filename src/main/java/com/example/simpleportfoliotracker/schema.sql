096d7b2fbb81dd30969a89625b69205159c8f043
Terence Tan <daterence@gmail.com>
2022-03-08 08:23:04 +0800

    final commit

--- src/main/java/com/example/simpleportfoliotracker/schema.sql
+++ src/main/java/com/example/simpleportfoliotracker/schema.sql
@@ -1,41 +1,43 @@
-drop table if exists description;
-drop table if exists portfolio;
-drop table if exists user;
+drop database simplePortfolioTracker;
+create database simplePortfolioTracker;
 
-create table if not exists user
+use simplePortfolioTracker;
+
+create table if not exists users
 (
     username varchar(64)  not null primary key ,
     password varchar(256) not null,
     email    varchar(64)  not null,
     roles    varchar(64)  not null
-    );
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO simplePortfolioTracker.user (username, password, email, roles)
-VALUES ('foo', 'foo', 'foo@email.com', 'ROLE_USER');
-INSERT INTO simplePortfolioTracker.user (username, password, email, roles)
-VALUES ('emily', 'emily', 'emily@email.com', 'ROLE_USER');
-# INSERT INTO simplePortfolioTracker.user (username, password, email, roles)
-# VALUES ('tom', 'tom', 'tom@email.com', 'ROLE_USER');
-# INSERT INTO simplePortfolioTracker.user (username, password, email, roles)
-# VALUES ('demo', 'demo', 'demo@email.com', 'ROLE_USER');
+INSERT INTO simplePortfolioTracker.users (username, password, email, roles)
+VALUES ('fooo', 'fooo@123', 'foo@email.com', 'ROLE_USER');
+INSERT INTO simplePortfolioTracker.users (username, password, email, roles)
+VALUES ('emily', 'emily@123', 'emily@email.com', 'ROLE_USER');
+INSERT INTO simplePortfolioTracker.users (username, password, email, roles)
+VALUES ('jane', 'jane@123', 'jane@email.com', 'ROLE_USER');
 
 create table portfolio
 (
-    id int not null auto_increment primary key ,
-    symbol     varchar(64) not null ,
+    id bigint not null auto_increment primary key ,
+    symbol  varchar(64) not null ,
     username   varchar(64) null, -- foreign key
     quantity   int,
     unit_price float,
-    constraint fk_portfolio foreign key (username) references user (username)
-);
+    constraint fk_portfolio foreign key (username) references users (username)
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-# create table description (
-#                              id int not null primary key , -- foreign key
-#                              symbol varchar(64) not null ,
-#                              url varchar(256) not null ,
-#                              name varchar(64) not null ,
-#                              exchange varchar(64) not null ,
-#                              industry varchar(64) not null ,
-#                              website varchar(256) not null,
-#                              constraint fk_desc foreign key (id) references portfolio(id)
-# );
+INSERT INTO simplePortfolioTracker.portfolio (symbol, username, quantity, unit_price) VALUES ('TSLA', 'fooo', 100, 420.69);
+
+INSERT INTO simplePortfolioTracker.portfolio (symbol, username, quantity, unit_price) VALUES ('AMZN', 'fooo', 30, 1509.84);
+
+INSERT INTO simplePortfolioTracker.portfolio (symbol, username, quantity, unit_price) VALUES ('AAPL', 'fooo', 150, 75.87);
+
+INSERT INTO simplePortfolioTracker.portfolio (symbol, username, quantity, unit_price) VALUES ('MSFT', 'fooo', 200, 178.92);
+
+INSERT INTO simplePortfolioTracker.portfolio (symbol, username, quantity, unit_price) VALUES ('PLTR', 'fooo', 12, 1100);
+
+INSERT INTO simplePortfolioTracker.portfolio (symbol, username, quantity, unit_price) VALUES ('TSLA', 'emily', 30, 487.21);
+
+INSERT INTO simplePortfolioTracker.portfolio (symbol, username, quantity, unit_price) VALUES ('MSFT', 'emily', 150, 150);
